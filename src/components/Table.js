89f6b6df10f6308f9e23008779e6f334f2eaee03
Nathan Vitor <nathanvitor38@gmail.com>
2022-08-09 15:26:46 -0300

    Requisito 7: Aprovado pelo Cypress

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,66 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Table extends Component {
+class Table extends Component {
+  expensesRender = () => {
+    const { expenses } = this.props;
+    return expenses
+      .map(({ id, value, currency, exchangeRates, method, tag, description }) => (
+        <tr key={ id }>
+          <td>{ description }</td>
+          <td>{ tag }</td>
+          <td>{ method }</td>
+          {/* valor */}
+          <td>{ ((Number(value) * 100) / 100).toFixed(2) }</td>
+          {/* moeda */}
+          <td>{ exchangeRates[currency].name }</td>
+          {/* cambio utilizado */}
+          <td>{ (Number(exchangeRates[currency].ask)).toFixed(2) }</td>
+          {/* valor convertido */}
+          <td>
+            { (Math
+              .round(Number(value) * Number(exchangeRates[currency].ask) * 100) / 100)
+              .toFixed(2) }
+          </td>
+          {/* moeda de conversão */}
+          <td>Real Brasileiro</td>
+        </tr>
+      ));
+  };
+
   render() {
     return (
       <div>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.expensesRender() }
+          </tbody>
         </table>
       </div>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (store) => ({
+  expenses: store.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
